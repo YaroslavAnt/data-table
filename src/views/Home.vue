42ad6cdce2b5a50bf18ff421dd0ee31a9a2998a6
YaroslavAnt <antonchyk.yar@gmail.com>
2020-04-08 18:00:54 +0300

    add table

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,60 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <v-data-table
+      :headers="headers"
+      :items="getProductsForTable"
+      :server-items-length='productsData.total'
+      :currentPage.sync="currentPage"
+      :pageCount='productsData.last_page'
+      hide-default-footer
+      class="elevation-1"
+    >
+      <template v-slot:top>
+        <v-toolbar
+          flat
+          color="white"
+        >
+          <v-toolbar-title>Категория: {{getCategoryFromParams}}</v-toolbar-title>
+          <v-divider
+            class="mx-4"
+            inset
+            vertical
+          ></v-divider>
+          <v-spacer></v-spacer>
+          <v-dialog
+            v-model="dialog"
+            max-width="500px"
+          >
+            <template v-slot:activator="{ on }">
+              <v-btn
+                v-on="on"
+                color="primary"
+                dark
+                class="mb-2"
+                @click="addItem()"
+              >Добавить товар</v-btn>
+            </template>
+            
+          </v-dialog>
+        </v-toolbar>
+      </template>
+
+      
+
+      
+
+      <template v-slot:item.action="{ item }">
+        <v-icon
+          small
+          class="mr-2"
+          @click="editItem(item)"
+        >mdi-pencil</v-icon>
+        <v-icon
+          small
+          @click="deleteItem(item)"
+        >mdi-delete</v-icon>
+      </template>
+    </v-data-table>
   </div>
 </template>
 
